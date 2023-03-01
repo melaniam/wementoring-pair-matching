b3f901ecf3c7d604fc8093d4efcd8c157fb47085
melaniam <melania.silvia.moldovan@gmail.com>
2023-03-01 21:10:44 +0000

    Update mapping.mjs to match three-pass rules

--- src/mapping.mjs
+++ src/mapping.mjs
@@ -28,15 +28,15 @@ const TYPES_OF_IC = {
 const TYPES_OF_MANAGERS = {
     firstLevelManager: {
         label: 'First level Manager (managing individual contributors, for example Manager)',
-        index: 1,
+        index: 4,
     },
     secondLevelManager: {
         label: 'Second level Manager (managing managers, for example roles such as Head of, Director)',
-        index: 2,
+        index: 5,
     },
     thirdLevelManager: {
         label: 'Third level Manager (managing managers of managers, for example Director, VP,  CxO)',
-        index: 3,
+        index: 6,
     },
     other: {
         label: 'Other',
@@ -52,49 +52,7 @@ const getTypeOfManager = (person) => {
     return Object.values(TYPES_OF_MANAGERS).find((type) => type.label === person.typeOfManager.trim());
 };
 
-const getTopicsOnWhichMenteeCanBeMentoredByMentor = (mentor, mentee, relaxedConditions) => {
-    // do not match members of the same company
-    if (mentor.workplace === mentee.workplace) {
-        return [];
-    }
-
-    // Mentors and Mentees should have the same area of expertise
-    if (mentor.workingArea !== mentee.workingArea) {
-        return [];
-    }
-
-    // Mentors and Mentees should be on the same track ( IC or Manager)
-    const mentorTypeOfIC = getTypeOfIndividualContributor(mentor);
-    const mentorTypeOfManager = getTypeOfManager(mentor);
-
-    const menteeTypeOfIC = getTypeOfIndividualContributor(mentee);
-    const menteeTypeOfManager = getTypeOfManager(mentee);
-
-    if (!relaxedConditions && ((mentorTypeOfIC && menteeTypeOfManager) || (mentorTypeOfManager && menteeTypeOfIC))) {
-        return [];
-    }
-
-    // When conditions are strict,
-    // Mentor should have a higher level of seniority than the mentee with a degree of 1, except for Other
-    const mentorTypeOfRole = mentorTypeOfIC || mentorTypeOfManager || {};
-    const menteeTypeOfRole = menteeTypeOfIC || menteeTypeOfManager || {};
-
-    // if they didn't choose "Other", look at the seniority level
-    // else try to map everyone to everyone - we need to figure out manually what level of seniority to consider
-    if (mentorTypeOfRole.index && menteeTypeOfRole.index) {
-        const levelDiff = mentorTypeOfRole.index - menteeTypeOfRole.index;
-        if (!relaxedConditions) {
-            if (levelDiff !== 1) {
-                return [];
-            }
-        } else {
-            // when conditions are more relaxed, the level of seniority can differ a bit more
-            if (levelDiff < 0 || levelDiff > 2) {
-                return [];
-            }
-        }
-    }
-
+const getTopicsOnWhichMenteeCanBeMentoredByMentor = (mentor, mentee) => {
     const mentorTopics = mentor.topicsToMentorOn.split(',').map((topic) => topic.trim());
     const menteeTopics = mentee.topicsToBeMentoredOn.split(',').map((topic) => topic.trim());
 
@@ -105,22 +63,61 @@ const getTopicsOnWhichMenteeCanBeMentoredByMentor = (mentor, mentee, relaxedCond
     return matchingMentorTopics;
 };
 
+const isValidPair = (mentor, mentee, conditionsAreStrict) => {
+    // do not match members of the same company
+    if (mentor.workplace === mentee.workplace) {
+        return false;
+    }
+
+    // Mentors and Mentees should have the same area of expertise
+    if (mentor.workingArea !== mentee.workingArea) {
+        return false;
+    }
+
+    // Mentors and Mentees should be on the same track ( IC or Manager)
+    const mentorTypeOfIC = getTypeOfIndividualContributor(mentor);
+    const mentorTypeOfManager = getTypeOfManager(mentor);
+
+    const menteeTypeOfIC = getTypeOfIndividualContributor(mentee);
+    const menteeTypeOfManager = getTypeOfManager(mentee);
+
+    const mentorSeniority = mentorTypeOfManager || mentorTypeOfIC || {};
+    const menteeSeniority = menteeTypeOfManager || menteeTypeOfIC || {};
+
+    // if they didn't choose "Other", look at the seniority level
+    // else try to map everyone to everyone - we need to figure out manually what level of seniority to consider
+    if (mentorSeniority.index && menteeSeniority.index) {
+        const levelDiff = mentorSeniority.index - menteeSeniority.index;
+        if (conditionsAreStrict) {
+            // When conditions are strict,
+            // Mentor should have a higher level of seniority than the mentee with a degree of 1, except for Other
+            if (levelDiff !== 1) {
+                return false;
+            }
+        } else {
+            // when conditions are more relaxed, the level of seniority can differ a bit more
+            if (levelDiff < 0 || levelDiff > 2) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
 export const mapMenteesToMentors = (mentors, mentees) => {
     const results = [];
 
     // First pass
     mentors.forEach((mentor) => {
         mentees.forEach((mentee) => {
-            const topics = getTopicsOnWhichMenteeCanBeMentoredByMentor(mentor, mentee);
-
-            if (topics.length > 0) {
+            if (isValidPair(mentor, mentee, true)) {
                 // TODO: update with more mentor and mentee details
                 results.push({
                     mentor: mentor.name,
                     mentorRole: mentor.role,
                     mentorAreaOfExpertise: mentor.workingArea,
                     mentorTopicsLong: mentor.topicsToMentorOn,
-                    commonTopics: topics,
                     mentee: mentee.name,
                     menteeRole: mentee.role,
                     menteeTopicsLong: mentee.topicsToBeMentoredOn,
@@ -134,14 +131,53 @@ export const mapMenteesToMentors = (mentors, mentees) => {
     });
 
     // Second pass
+    console.log('Second Pass');
     mentors.forEach((mentor) => {
         if (mentor.assignedOnce && mentor.numberOfMentees === '1') {
             return;
         }
+        console.log(mentor.name);
 
         const mentorWantsToBeContacted = mentor.numberOfMentees.includes('contact');
 
         mentees.forEach((mentee) => {
+            const validPair = isValidPair(mentor, mentee);
+
+            if (validPair && !mentor.assignedTwice && !mentee.assigned) {
+                mentor.assignedTwice = true;
+                mentee.assigned = true;
+
+                results.push({
+                    mentor: mentor.name,
+                    mentorRole: mentor.role,
+                    mentorAreaOfExpertise: mentor.workingArea,
+                    mentorTopicsLong: mentor.topicsToMentorOn,
+                    mentee: mentee.name,
+                    menteeRole: mentee.role,
+                    menteeTopicsLong: mentee.topicsToBeMentoredOn,
+                    menteeLearningGoal: mentee.learningGoal,
+                    contact: mentorWantsToBeContacted ? 'contact mentor!' : '',
+                });
+            }
+        });
+    });
+
+    // Third Pass
+    console.log('Third Pass');
+
+    mentors.forEach((mentor) => {
+        if ((mentor.assignedOnce && mentor.numberOfMentees === '1') || mentor.assignedTwice) {
+            return;
+        }
+        console.log(mentor.name);
+
+        const mentorWantsToBeContacted = mentor.numberOfMentees.includes('contact');
+
+        mentees.forEach((mentee) => {
+            if (mentor.workplace === mentee.workplace) {
+                return;
+            }
+
             const topics = getTopicsOnWhichMenteeCanBeMentoredByMentor(mentor, mentee, true);
 
             if (topics.length > 0 && !mentor.assignedTwice && !mentee.assigned) {
